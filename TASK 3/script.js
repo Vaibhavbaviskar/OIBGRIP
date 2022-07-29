b4e0d5f92b7f5a76e234133e51598f62cd0ae8e5
Vaibhavbaviskar <106547655+Vaibhavbaviskar@users.noreply.github.com>
2022-07-29 14:21:50 +0530

    Add files via upload

---
+++ TASK 3/script.js
@@ -0,0 +1,13 @@
+let fahrenheit = document.getElementById("fahrenheit");
+let celsius = document.getElementById("celsius");
+
+function celToFar(){
+    let output = ( parseFloat(celsius.value) * 9/5 ) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+}
+
+function farToCel(){
+    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
+    celsius.value = parseFloat( output.toFixed(2));
+    console.log(output);
+}
